e4b8ccbcd36ddebe2611d0b710985fd6aea30352
mourad-tiferras2002 <76862748+mourad-tiferras2002@users.noreply.github.com>
2021-01-02 09:03:32 +0100

    Add files via upload
    Hello World this is a simple project powered by Mourad TIFERRAS . Also, this project can help you to learn programming , web , database , mobile dev. and free pdf e-books ... for free and online with yourself You can have the mindset of self - educated ( self-training , auto-formation )

---
+++ ofppt self-edu.py
@@ -0,0 +1,47 @@
+print("             ----------------------------------------------------------------------------     ")
+print(" \n                ======       ========       =======-     =======-       ==========   ")
+print("               #      #     #               #     #/     #     #/           #        ")
+print("               #      #     # =====        #--===/      #--===/             #        ")
+print("               #      #     #              #            #                   #        ")
+print("                ======      #              #            #                   #        ")
+print('                                                                                 of Nador City')
+print("               \n                    This project powred by: TIFERRAS__Mourad  To share IT :")
+print("        -----------------------------------------------------------------------------------------    ")
+print("             ##############################################################################")
+print("         ######################################################################################")
+
+homme = (" \n Information ! : In this project you can find only domaine name of web sites for self-education for free  :)  \n \n       Select just your number choice: \n \n  [1]: Learn Computer Software : \n  [2]: free PDF Books : \n  [3] About the author :    ")
+print(homme)
+x = int(input(" \n  =>  "))
+while x :
+ if x == int(1) :
+    print("In this web sites you can learn [Coding, Data_Structure, Data Bases, Machine learning, Web & Mobile applications...] for free: \n \n    .SOLO LEARN \n    .W3school \n    .TutorialsPoint \n    .FreeCodeCamp , you can find it  also youtube chanel ")
+    x = int(input(" \n  =>  "))
+
+ if x == int(2) :
+    print("  In this web site you can find many free PDF books: \n    \n   .PDF Drive ")
+    x = int(input(" \n  =>  "))
+
+ if x == int(3) :
+     print(" Hello World! I'm MOURAD with a passion for all things technology. I fell in love with technology when I was 14 years old in 2016 . Also I have been an Ethical Hacker since 2019. I don't care about the academic diploma because i want to move up to the next level then i don't rely only on academic programme and i think outside the box. In order to be perfect and to be in the ball in professional life. Because all my advanced skills is based from e-learning, Books, auto-formation and practice.  ")                                                                                                                                                  
+     x = int(input(" \n  =>  "))
+
+
+
+
+ 
+     
+
+
+ 
+  
+  
+  
+
+    
+
+    
+            
+
+    
+
